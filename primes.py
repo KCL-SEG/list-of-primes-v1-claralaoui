4cf3a265a18a50f1ad29626b115ef0093fbf09b7
Clara <clara.laoui@kcl.ac.uk>
2023-09-21 20:59:12 +0200

    first try

--- primes.py
+++ primes.py
@@ -2,5 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    
     list = []
+    next_number = 0
+    while len(list) < number_of_primes:
+        if is_prime(next_number):
+            list.append(next_number)
+        
+        next_number = next_number + 1
     return list
